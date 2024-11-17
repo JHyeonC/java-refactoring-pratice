53a5ce7c87967fdc9c9ce0b6705afacf55f99cfb
choejonghyeon <choijonghyen@gmail.com>
2024-11-17 15:43:43 +0900

    refactor: statement 메소드, 적립 포인트 계산 로직 메소드 추출 및 메소드 이동

--- src/Customer.java
+++ src/Customer.java
@@ -29,9 +29,7 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentalEnumeration.nextElement();
 
-            frequentRenterPoints ++;
-            if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                each.getDaysRented() > 1) frequentRenterPoints ++;
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             result  = "\t" + each.getMovie().getTitle() + "\t" +
                 String.valueOf(each.getCharge()) + "\n";
