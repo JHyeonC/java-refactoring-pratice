d127ca5815316c9392bd2a405e026a1d03337ac4
choejonghyeon <choijonghyen@gmail.com>
2024-11-17 16:02:58 +0900

    refactor: 임시 변수 제거하기

--- src/Customer.java
+++ src/Customer.java
@@ -19,25 +19,38 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration rentalEnumeration = rentals.elements();
 
         String result = getName() + "고객님의 대여 기록\n";
 
         while(rentalEnumeration.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentalEnumeration.nextElement();
-
-            frequentRenterPoints += each.getFrequentRenterPoints();
 
             result  = "\t" + each.getMovie().getTitle() + "\t" +
                 String.valueOf(each.getCharge()) + "\n";
 
-            totalAmount += each.getCharge();
+            result += "누적 대여로: " + String.valueOf(getTotalCharge()) + "\n";
+            result += "적립 포인트: " + String.valueOf(getTotalFrequentRenterPoints());
+        }
+        return result;
+    }
 
-            result += "누적 대여로: " + String.valueOf(totalAmount) + "\n";
-            result += "적립 포인트: " + String.valueOf(frequentRenterPoints);
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentalEnumeration = rentals.elements();
+        while(rentalEnumeration.hasMoreElements()) {
+            Rental each = (Rental) rentalEnumeration.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentalEnumeration = rentals.elements();
+        while(rentalEnumeration.hasMoreElements()) {
+            Rental each = (Rental) rentalEnumeration.nextElement();
+            result += each.getFrequentRenterPoints();
         }
         return result;
     }
