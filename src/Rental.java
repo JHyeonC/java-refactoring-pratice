3a44daf88d7720b5687d41ec85f24ccf77dc91ff
choejonghyeon <choijonghyen@gmail.com>
2024-11-27 21:35:24 +0900

    refactor: 4인방의 상태 패턴 적용

--- src/Rental.java
+++ src/Rental.java
@@ -16,32 +16,10 @@ public class Rental {
         return this.movie;
     }
 
-    public double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if(getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if(getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
-    }
+    public double getCharge() { return this.movie.getCharge(this.daysRented); }
 
     public int getFrequentRenterPoints() {
-        if((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            getDaysRented() > 1)
-            return 2;
-        else
-            return 1;
+        return this.movie.getFrequentRenterPoints(this.daysRented);
     }
 
 }
